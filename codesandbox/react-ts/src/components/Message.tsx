c5d96138ff64d66fdff7e0df1d8e61c12ccfa7cf
Jan <uicoded@gmail.com>
2024-11-17 21:00:08 -0500

    Define the Message Props type inline

--- codesandbox/react-ts/src/components/Message.tsx
+++ codesandbox/react-ts/src/components/Message.tsx
@@ -3,14 +3,20 @@ import "./Message.css"; // Optional, if you'd like to style the component
 
 export type MessageType = "warning" | "info" | "error" | "success";
 
-interface MessageProps {
-  type?: MessageType;
-  children: React.ReactNode;
-}
+// interface MessageProps {
+//   type?: MessageType;
+//   children: React.ReactNode;
+// }
 
-export default function Message({ type, children }: MessageProps) {
+// export default function Message({ type, children }: MessageProps) {
+//   const typeClass = `message-${type}`;
+
+//   return <div className={`message ${typeClass}`}>{children}</div>;
+// }
+
+// Either way it is upto you.
+export default function Message({ type, children }: {type?: MessageType, children: React.ReactNode}) {
   const typeClass = `message-${type}`;
 
   return <div className={`message ${typeClass}`}>{children}</div>;
 }
-
