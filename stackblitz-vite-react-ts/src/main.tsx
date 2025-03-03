6bf8452c505a23a43140c8b577dbbf67a5a70981
Jan <uicoded@gmail.com>
2025-03-03 16:22:44 -0500

    error boundaries example

--- stackblitz-vite-react-ts/src/main.tsx
+++ stackblitz-vite-react-ts/src/main.tsx
@@ -4,6 +4,7 @@ import './index.css'
 // Swap this 👇 for the example app that you're testing
 import App from './App_default.tsx'
 // import App from './examples/App_forms-uncontrolled-inputs-default-value'
+// import App from './examples/App_error-boundary.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
