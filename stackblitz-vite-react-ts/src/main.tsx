221c682d41fc058205980d4285e7c51c0f547147
Jan <uicoded@gmail.com>
2025-03-06 10:40:06 -0500

    category updates in main

--- stackblitz-vite-react-ts/src/main.tsx
+++ stackblitz-vite-react-ts/src/main.tsx
@@ -3,13 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // Swap this 👇 for the example app that you're testing
 import App from './App_default.tsx'
-// import App from './examples/App_forms-uncontrolled-inputs-default-value'
+// import App from './examples/forms/App_forms-uncontrolled-inputs-default-value'
 // import App from './examples/App_error-boundary.tsx'
-// import App from './examples/App_hooks-set-state.tsx'
-// import App from './examples/App_3-ways-adding-style.tsx'
+// import App from './examples/hooks/App_hooks-use-state.tsx'
 // import App from './examples/App_using-keys.tsx'
-// import './examples/App_styled-box.css';
-// import App from './examples/App_styled-box.tsx'
+// import App from './examples/styles/App_3-ways-adding-style.tsx'
+// import './examples/styles/App_styled-box.css';
+// import App from './examples/styles/App_styled-box.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
